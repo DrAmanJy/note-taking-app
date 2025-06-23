bfdcc56d03e45ae508f96a06db83259f160ae97a
Aman Lathar <dramanjy0770@gmail.com>
2025-06-23 18:29:55 +0530

    added auth controller to routes

--- Back-end/src/routes/authRoutes.js
+++ Back-end/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
+import { login, signup } from "../controllers/authController.js";
 const authRoutes = express.Router();
-// authRoutes.post("/register");
-// authRoutes.post("/login");
+authRoutes.post("/signup", signup);
+authRoutes.post("/login", login);
 export default authRoutes;
