274b2913a6949f4c914f22e2f86bd25a6a908d16
Aman Lathar <dramanjy0770@gmail.com>
2025-06-23 12:03:10 +0530

    added signup and  login controller

---
+++ Back-end/src/controllers/authController.js
@@ -0,0 +1,56 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please provide all fields" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = User.create({ name, email, password: hashedPassword });
+    return res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        type: user.type,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please provide all fields" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "incorrect password" });
+    }
+    res.status(200).json({
+      message: "User login successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        type: user.type,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
