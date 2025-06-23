51ddf06b236ea4b243f5bc7c4a56e3e4e4815944
Aman Lathar <dramanjy0770@gmail.com>
2025-06-23 20:11:57 +0530

    added responsive tasks container

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,5 +1,92 @@
 function App() {
-  return <></>;
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-bl from-sky-500 via-sky-700 to-sky-900">
+      <div className="flex flex-col items-center gap-5 pt-24 px-4">
+        {/* Top Section */}
+        <div className="w-full max-w-6xl min-h-[20vh] bg-gray-300 rounded-xl"></div>
+
+        {/* Cards Section */}
+        <div className="w-full max-w-6xl min-h-[60vh] flex flex-wrap justify-center gap-5 py-5 md:py-10 rounded-xl">
+          <div className="w-full sm:w-[48%] xl:w-[32%] bg-red-50 rounded-lg  flex flex-col justify-between p-4">
+            <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-base mb-2">
+              abcdefghijklmnopqrstuvwxyza
+            </h2>
+            <p className="text-justify text-sm sm:text-base mb-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+              laudantium, officia asperiores recusandae quo ad, delectus animi
+              excepturi quos vitae qui unde voluptas eos aliquid eius quasi cum
+              nihil repellendus?
+            </p>
+            <div className="w-full flex justify-between gap-2">
+              <button className="flex-1 bg-red-500 text-white py-2 px-4 rounded">
+                Edit
+              </button>
+              <button className="flex-1 bg-gray-300 text-black py-2 px-4 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+          <div className="w-full sm:w-[48%] xl:w-[32%] bg-red-50 rounded-lg  flex flex-col justify-between p-4">
+            <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-base mb-2">
+              abcdefghijklmnopqrstuvwxyza
+            </h2>
+            <p className="text-justify text-sm sm:text-base mb-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+              laudantium, officia asperiores recusandae quo ad, delectus animi
+              excepturi quos vitae qui unde voluptas eos aliquid eius quasi cum
+              nihil repellendus?
+            </p>
+            <div className="w-full flex justify-between gap-2">
+              <button className="flex-1 bg-red-500 text-white py-2 px-4 rounded">
+                Edit
+              </button>
+              <button className="flex-1 bg-gray-300 text-black py-2 px-4 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+          <div className="w-full sm:w-[48%] xl:w-[32%] bg-red-50 rounded-lg  flex flex-col justify-between p-4">
+            <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-base mb-2">
+              abcdefghijklmnopqrstuvwxyza
+            </h2>
+            <p className="text-justify text-sm sm:text-base mb-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+              laudantium, officia asperiores recusandae quo ad, delectus animi
+              excepturi quos vitae qui unde voluptas eos aliquid eius quasi cum
+              nihil repellendus?
+            </p>
+            <div className="w-full flex justify-between gap-2">
+              <button className="flex-1 bg-red-500 text-white py-2 px-4 rounded">
+                Edit
+              </button>
+              <button className="flex-1 bg-gray-300 text-black py-2 px-4 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+          <div className="w-full sm:w-[48%] xl:w-[32%] bg-red-50 rounded-lg  flex flex-col justify-between p-4">
+            <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-base mb-2">
+              abcdefghijklmnopqrstuvwxyza
+            </h2>
+            <p className="text-justify text-sm sm:text-base mb-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+              laudantium, officia asperiores recusandae quo ad, delectus animi
+              excepturi quos vitae qui unde voluptas eos aliquid eius quasi cum
+              nihil repellendus?
+            </p>
+            <div className="w-full flex justify-between gap-2">
+              <button className="flex-1 bg-red-500 text-white py-2 px-4 rounded">
+                Edit
+              </button>
+              <button className="flex-1 bg-gray-300 text-black py-2 px-4 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
