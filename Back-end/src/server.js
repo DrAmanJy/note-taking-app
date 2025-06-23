58466c88507d9a0a28815d4f840434c5ac49b6b1
Aman Lathar <dramanjy0770@gmail.com>
2025-06-23 12:01:27 +0530

    added new library

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDb from "./config/db.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
-
+connectDb();
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
