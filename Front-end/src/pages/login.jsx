693b9f0b4cb89576b0f8ca3251731ab1e9b7eea9
Aman Lathar <dramanjy0770@gmail.com>
2025-06-27 19:57:24 +0530

    added login logic

--- Front-end/src/pages/login.jsx
+++ Front-end/src/pages/login.jsx
@@ -1,27 +1,52 @@
 import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import loginSchema from "../schemas/loginSchema";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const { user, login, loading, fetchError } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-sky-500 via-sky-700 to-sky-900 px-4">
       <div className="w-full max-w-md bg-white rounded-xl shadow-md p-8 space-y-6">
         <h2 className="text-2xl font-bold text-center text-sky-700">Login</h2>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit(login)} className="space-y-4">
           <input
             type="email"
             placeholder="Email"
+            {...register("email")}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
           <input
             type="password"
             placeholder="Password"
+            {...register("password")}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
           <button
+            disabled={loading}
             type="submit"
             className="w-full bg-sky-600 text-white py-2 rounded-md hover:bg-sky-700 transition"
           >
-            Login
+            {loading ? "loading" : "Login"}
           </button>
+          {fetchError && (
+            <p className="text-red-500 text-sm">{fetchError.message}</p>
+          )}
         </form>
         <p className="text-center text-sm text-gray-600">
           Don't have an account?{" "}
