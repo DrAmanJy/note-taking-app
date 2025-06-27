2fe62a11af513694e9ba8e4906bc3f3afa7bd79f
Aman Lathar <dramanjy0770@gmail.com>
2025-06-27 19:58:02 +0530

    added signup logic

--- Front-end/src/pages/signup.jsx
+++ Front-end/src/pages/signup.jsx
@@ -1,37 +1,70 @@
 import { Link } from "react-router";
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import signupSchema from "../schemas/signupSchema";
+import { useAuth } from "../context/AuthContext";
 const Signup = () => {
+  const { user, signup, loading, fetchError } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+    mode: "onTouched",
+  });
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-sky-500 via-sky-700 to-sky-900 px-4">
       <div className="w-full max-w-md bg-white rounded-xl shadow-md p-8 space-y-6">
         <h2 className="text-2xl font-bold text-center text-sky-700">Sign Up</h2>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit(signup)} className="space-y-4">
           <input
             type="text"
             placeholder="Full Name"
+            {...register("name")}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )}
           <input
             type="email"
             placeholder="Email"
+            {...register("email")}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
           <input
             type="password"
             placeholder="Password"
+            {...register("password")}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
           <input
             type="password"
+            {...register("confirmPassword")}
             placeholder="Confirm Password"
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
+          {errors.confirmPassword && (
+            <p className="text-red-500 text-sm">
+              {errors.confirmPassword.message}
+            </p>
+          )}
           <button
+            disabled={loading}
             type="submit"
             className="w-full bg-sky-600 text-white py-2 rounded-md hover:bg-sky-700 transition"
           >
-            Create Account
+            {loading ? "loading" : "Create Account"}
           </button>
+          {fetchError && <p className="text-red-500 text-sm">{fetchError}</p>}
         </form>
         <p className="text-center text-sm text-gray-600">
           Already have an account?{" "}
