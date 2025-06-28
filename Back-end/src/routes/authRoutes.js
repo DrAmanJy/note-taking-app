4742271a90520c0258463baf4154446ae1733711
Aman Lathar <dramanjy0770@gmail.com>
2025-06-28 12:51:33 +0530

    added me route

--- Back-end/src/routes/authRoutes.js
+++ Back-end/src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { login, signup } from "../controllers/authController.js";
+import {
+  getCurrentUser,
+  login,
+  signup,
+} from "../controllers/authController.js";
 const authRoutes = express.Router();
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
+authRoutes.get("/me", getCurrentUser);
 export default authRoutes;
