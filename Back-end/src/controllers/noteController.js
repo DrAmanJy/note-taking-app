9878db027b7c51a120b9def744763b9aa484cf26
Aman Lathar <dramanjy0770@gmail.com>
2025-06-28 12:49:57 +0530

    make all notes save with userId

--- Back-end/src/controllers/noteController.js
+++ Back-end/src/controllers/noteController.js
@@ -2,10 +2,7 @@ import Note from "../models/noteModel.js";
 
 export const getNotes = async (req, res) => {
   try {
-    const notes = await Note.find();
-    if (notes.length === 0) {
-      return res.status(404).json({ message: "No notes found" });
-    }
+    const notes = await Note.find({ userId: req.user._id });
     return res.status(200).json({ notes });
   } catch (err) {
     return res.status(500).json({ message: err.message });
@@ -13,7 +10,10 @@ export const getNotes = async (req, res) => {
 };
 export const getNote = async (req, res) => {
   try {
-    const note = await Note.findById(req.params.id);
+    const note = await Note.findOne({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
     if (!note) {
       return res.status(404).json({ message: "Note not found" });
     }
@@ -25,14 +25,18 @@ export const getNote = async (req, res) => {
 export const addNote = async (req, res) => {
   const { title, description } = req.body;
   try {
-    if (!title) {
-      return res.status(400).json({ message: "Please provide title" });
-    }
-    if (!description) {
-      return res.status(400).json({ message: "Please provide description" });
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description required" });
     }
 
-    const newNote = await Note.create({ title, description });
+    const newNote = await Note.create({
+      title,
+      description,
+      userId: req.user._id,
+    });
+
     return res.status(201).json({ message: "Note added", note: newNote });
   } catch (err) {
     return res.status(500).json({ message: err.message });
@@ -40,21 +44,18 @@ export const addNote = async (req, res) => {
 };
 export const updateNote = async (req, res) => {
   const { title, description, completed } = req.body;
-  try {
-    if (!title || !description) {
-      return res
-        .status(400)
-        .json({ message: "Title and description are required" });
-    }
 
-    const updatedNote = await Note.findByIdAndUpdate(
-      req.params.id,
+  try {
+    const updatedNote = await Note.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user._id },
       { title, description, completed },
       { new: true }
     );
 
     if (!updatedNote) {
-      return res.status(404).json({ message: "Note not found" });
+      return res
+        .status(404)
+        .json({ message: "Note not found or unauthorized" });
     }
 
     return res.status(200).json({ message: "Note updated", note: updatedNote });
@@ -64,9 +65,15 @@ export const updateNote = async (req, res) => {
 };
 export const deleteNote = async (req, res) => {
   try {
-    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    const deletedNote = await Note.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
     if (!deletedNote) {
-      return res.status(404).json({ message: "Note not found" });
+      return res
+        .status(404)
+        .json({ message: "Note not found or unauthorized" });
     }
 
     return res.status(200).json({ message: "Note deleted" });
