4fe7007b95207a96112493102a99e95ead67e460
Aman Lathar <dramanjy0770@gmail.com>
2025-06-28 12:47:49 +0530

    added environment variables

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -4,15 +4,22 @@ import cors from "cors";
 import connectDb from "./config/db.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5500;
+
+// Connect DB
 connectDb();
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
@@ -21,6 +28,7 @@ app.use(
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", noteRoutes);
 
-app.listen(5500, () => {
-  console.log("Server is running on http://localhost:5500/");
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
