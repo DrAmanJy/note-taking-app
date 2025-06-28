c61d1b3b65139e8a2c074d7800f20d05413f4f5c
Aman Lathar <dramanjy0770@gmail.com>
2025-06-28 12:52:12 +0530

    added logout btn

--- Front-end/src/components/navbar.jsx
+++ Front-end/src/components/navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
 import { Link } from "react-router";
+import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useAuth();
   return (
     <nav className="w-full bg-white shadow-md fixed top-0 left-0 z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
@@ -9,12 +10,18 @@ const Navbar = () => {
           TaskManager
         </Link>
         <div className="flex gap-4 items-center">
-          <Link
-            to={"/auth?type=login"}
-            className="bg-sky-600 text-white px-4 py-2 rounded-md hover:bg-sky-700 transition"
-          >
-            Login
-          </Link>
+          {user ? (
+            <button className="bg-sky-600 text-white px-4 py-2 rounded-md hover:bg-sky-700 transition">
+              Logout
+            </button>
+          ) : (
+            <Link
+              to={"/auth?type=login"}
+              className="bg-sky-600 text-white px-4 py-2 rounded-md hover:bg-sky-700 transition"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </div>
     </nav>
